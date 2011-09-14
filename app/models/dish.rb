d6bc963c18e07ff1b1fc7d54fe2f23d20b445b1d
jenjenbirdie <jennifer.birdie@gmail.com>
2011-09-14 10:46:09 -0700

    Done with custom helpers

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,15 +1,15 @@
 class Dish < ActiveRecord::Base
-	validates :title, :presence => true
+	validates :title, :presence => true, :length => { :maximum => 150}
 	validates :ingredients, :presence => true
 	validates :direction, :presence => true
-	validates :duration, :presence => true
+	validates_numericality_of :duration, :presence => true, :greater_than => 0
 	
 	belongs_to :user
 	has_many :comments
 
-	CATEGORY1_TYPES = ["Chicken", "Beef", "Vegetable", "Seafood", "Veal", "Bakery Goods", "Others"]
-	CATEGORY2_TYPES = ["Breakfast", "Main Meal", "Snack", "Dessert", "Others"]
-	CATEGORY3_TYPES = ["American", "Vietnamese", "Chinese", "Japanese", "Indian", "Mexican", "European", "Filipino", "Others", "unknown"]
+	CATEGORY1_TYPES = ["Chicken", "Beef", "Pork", "Vegetable", "Seafood", "Veal", "Bakery Goods", "Others"]
+	CATEGORY2_TYPES = ["Breakfast", "Main Course", "Side Dish", "Soup", "Salad", "Burger & Sandwich", "Pasta", "Snack", "Dessert", "Others"]
+	CATEGORY3_TYPES = ["American", "Vietnamese", "Chinese", "German", "Japanese", "Indian", "Mexican", "European", "Filipino", "Others", "unknown"]
 
 	def owned_by?(owner)
 		return false unless owner.is_a? User
